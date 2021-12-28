976bf1f0e17278b10dd60a1409c9eb3af9c3a8c7
nepp-plus <nepp.plus@gmail.com>
2021-12-28 12:15:28 +0900

    학생클래스 메쏘드 2개 추가
    - 나이 계산 -> 리턴
    - 가공된 문구로 정보 출력

--- datas/student.py
+++ datas/student.py
@@ -6,3 +6,12 @@ class Student:
         self.name = name
         self.birth_year = int(birth_year)  # str이 들어온 경우 대비
         self.address = address
+        
+    # 나이를 계산해주는 기능 추가. (2021년 기준)
+    def get_age(self):
+        return 2021 - self.birth_year + 1
+    
+    # 정보를 가공해서 출력 (print) 기능. - return X
+    # 조경진(34세) - 서울시 동대문구   이 양식으로.
+    def print_student_info(self):
+        print( f'{self.name}({self.get_age()}세) - {self.address}' )
