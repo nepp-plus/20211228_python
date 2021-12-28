26117e53accfdf904a7387a349925249d4ac9668
nepp-plus <nepp.plus@gmail.com>
2021-12-28 11:15:37 +0900

    무한반복으로 -> 한줄씩 읽는 행위를 반복.

--- main03.py
+++ main03.py
@@ -2,11 +2,18 @@
 
 my_file = open('c:\\202112_CKJ_Python\\file_test\\my_test3.txt', 'r') # r: read 읽어오기 모드.
 
-line = my_file.readline()
+# 파일 내용 전부 읽고싶다. => 몇줄이 전부일까? 알 수 없음.
+# 반복 횟수가 불명확한 반복. => while True :  + if break 용법 활용.
 
-print(line)
-
-line2 = my_file.readline()
-print(line2)
+while True:
+    line = my_file.readline()
+    
+    # 내용 출력 : 읽어온 내용이 있어야 의미있는 행위.
+    # 읽어온 내용에 아무것도 없다 : 반복 종료.
+    if not line:
+        print('파일을 끝까지 읽었습니다.')
+        break
+    
+    print(line, end='')
 
 my_file.close()
