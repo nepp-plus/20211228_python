a7f1b5f4ae9995118e365fd896a85ad0645dd488
nepp-plus <nepp.plus@gmail.com>
2021-12-28 10:32:03 +0900

    append 모드 체험

--- main01.py
+++ main01.py
@@ -1,6 +1,5 @@
-my_file = open('내파일.txt', 'w') # w : write 모드로 파일 열기.
+my_file = open('내파일.txt', 'a') # a : append 모드.
 
-my_file.write('안녕하세요.\n')
-my_file.write('내가 파이썬으로 만든\n첫번째 파일입니다.\n')
+my_file.write('한 줄 내용을 추가합니다.\n')
 
 my_file.close()
