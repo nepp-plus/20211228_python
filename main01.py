77b0cdd5abeb7753dca381395ed24fd5a8965cce
nepp-plus <nepp.plus@gmail.com>
2021-12-28 10:41:53 +0900

    상대 경로 체험 (1)
    - 내부 폴더로 들어가기

--- main01.py
+++ main01.py
@@ -1,7 +1,6 @@
-my_file = open('내파일.txt', 'a') # a : append 모드.
+my_file = open('test/내파일.txt', 'w') # a : append 모드.
 
 # 주석 추가 - 변경사항 발생
-my_file.write('두 줄 내용을 추가합니다.\n')
-my_file.write('두 줄 내용을 추가합니다.\n')
+my_file.write('폴더 내에 내 파일 추가\n')
 
 my_file.close()
